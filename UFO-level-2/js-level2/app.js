44a34cc59f35034b77ea16f1f038f7fb4bd8137d
Anna Bowers <eenabow@gmail.com>
2021-03-08 20:50:48 -0500

    Rearranging code from feedback

--- UFO-level-2/js-level2/app.js
+++ UFO-level-2/js-level2/app.js
@@ -5,11 +5,11 @@ var columns = ["datetime", "city", "state", "country", "shape", "minutes", "comm
 // Get a reference to the table body, user input values, and button in html
 var tbody = d3.select("tbody");
 var button = d3.select('#filter-btn');
-var inputDate = d3.select('#datetime').property("value").toLowerCase;
-var inputCity = d3.select('#city').property("value").toLowerCase;
-var inputState = d3.select('#state').property("value").toLowerCase;
-var inputCountry = d3.select('#country').property("value").toLowerCase;
-var inputShape = d3.select('#shape').property("value").toLowerCase;
+var inputDate = d3.select('#datetime');
+var inputCity = d3.select('#city');
+var inputState = d3.select('#state');
+var inputCountry = d3.select('#country');
+var inputShape = d3.select('#shape');
 
 // Input the data into index.html
 function buildTable(applicableData) {
@@ -23,43 +23,48 @@ function buildTable(applicableData) {
     });
 };
 
-// buildTable(tableData);
+buildTable(tableData);
+
+function filterTable() {
+    var dateValue = inputDate.property("value");
+    var cityValue = inputCity.property("value").toLowerCase();
+    var stateValue = inputState.property("value").toLowerCase();
+    var countryValue = inputCountry.property("value").toLowerCase();
+    var shapeValue = inputShape.property("value").toLowerCase();
+
+    // Prevent page from refreshing
+    // d3.event.preventDefault();
+
+    // Create a filter function for datetime
+
+    tbody.html("");
+
+    filteredData = tableData;
+    console.log(dateValue, cityValue, stateValue, countryValue, shapeValue)
+
+    if (dateValue) {
+        filteredData = filteredData.filter(row => row.datetime === dateValue);
+    };
+    if (cityValue) {
+        filteredData = filteredData.filter(row => row.city === cityValue);
+    };
+    if (stateValue) {
+        filteredData = filteredData.filter(row => row.state === stateValue);
+    };
+    if (countryValue) {
+        filteredData = filteredData.filter(row => row.country === countryValue);
+    };
+    if (shapeValue) {
+        filteredData = filteredData.filter(row => row.shape === shapeValue);
+    };
+
+    buildTable(filteredData);
+};
+
 
 // Assign handler function to objects in the HTML file == button and form
-button.on("click", function() {
-
-// Prevent page from refreshing
-d3.event.preventDefault();
-
-// Create a filter function for datetime
-
-tbody.html("");
-
-filteredData = tableData;
-
-if (inputDate) {
-    filteredData = filteredData.filter(row => row.datetime === inputDate);
-};
-if (inputCity) {
-    filteredData = filteredData.filter(row => row.city === inputCity);
-};
-if (inputState) {
-    filteredData = filteredData.filter(row => row.state === inputState);
-};
-if (inputCountry) {
-    filteredData = filteredData.filter(row => row.country === inputCountry);
-};
-if (inputShape) {
-    filteredData = filteredData.filter(row => row.shape === inputShape);
-};
-filteredData.forEach((UFOsighting) => {
-    var row = tbody.append("tr");
-    Object.entries(UFOsighting).forEach(([key, value]) => {
-        var cell = row.append("td");
-        cell.text(value);
-    });
-});
-});
+button.on("click", filterTable);
 
 
-    // buildTable(filteredData);
+
+// buildTable(filteredData);
